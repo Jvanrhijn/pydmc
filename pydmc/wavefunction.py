6f1c5be297a76e29f3c55019107183359967bcff
Jesse van Rhijn <jesse.v.rhijn@gmail.com>
2020-08-18 09:37:49 +0200

    Add WaveFunction and Parametrized ABCs

---
+++ pydmc/wavefunction.py
@@ -0,0 +1,33 @@
+from abc import ABC, abstractmethod
+
+import numpy as np
+
+
+class WaveFunction(ABC):
+
+    @abstractmethod
+    def __call__(self, x):
+        pass
+
+    @abstractmethod
+    def gradient(self, x):
+        pass
+
+    @abstractmethod
+    def laplacian(self, x):
+        pass
+
+
+class Parametrized(ABC):
+
+    @abstractmethod
+    def parameters(self):
+        pass
+
+    @abstractmethod
+    def parameter_gradient(self):
+        pass
+
+    @abstractmethod
+    def set_parameters(self, ps):
+        pass
