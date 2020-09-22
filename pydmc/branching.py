Fix some averages (WIP)

pydmc/branching.py
@@ -21,8 +21,8 @@ class SRBrancher(Brancher):
     def perform_branching(self, walkers):
         weights = np.array([walker.weight for walker in walkers])
         weights /= weights.sum()
-        #global_weight = np.mean(weights)
-        global_weight = 1/len(walkers)
+        global_weight = np.mean(weights)
+        #global_weight = 1/len(walkers)
         confs = [walker.configuration for walker in walkers]
         new_confs = random.choices(confs, weights=weights, k=len(walkers))
         return [Walker(conf, global_weight) for conf in new_confs]

pydmc/forces.py
@@ -159,8 +159,6 @@ class DMCForcesInput:
         energy = np.mean(data["Local energy"])
 
         local_energy = data["Local energy"]
-        local_energy_sec = data["Local energy (secondary)"]
-        local_energy_sec_warp = data["Local energy (secondary, warp)"]
 
         s = data["S"]
         t = data["T"]
@@ -170,25 +168,30 @@ class DMCForcesInput:
         tsec_warp = data["T (secondary, warp)"]
 
         psi = data["Psi"]
-        psi_sec = data["Psi (secondary)"]
-        psi_sec_warp = data["Psi (secondary, warp)"]
+        #psi_sec = data["Psi (secondary)"]
+        #psi_sec_warp = data["Psi (secondary, warp)"]
         
-        logjac = data["Log Jacobian"]
+        el_times_jac_logderiv = data["E_L * grad_a Log Jacobian"]
+        jac_logderiv = data["grad_a Log Jacobian"]
         jac_partial_sum = data["Sum Log Jacobian"]
 
         # compute local e derivative
-        local_e_deriv = (local_energy_sec - local_energy) / da
-        local_e_deriv_warp = (local_energy_sec_warp - local_energy) / da
+        #local_e_deriv = (local_energy_sec - local_energy) / da
+        local_e_deriv = data["grad_a E_L"]
+        #local_e_deriv_warp = (local_energy_sec_warp - local_energy) / da
+        local_e_deriv_warp = data["grad_a E_L (warp)"]
 
         force_hf = (-local_e_deriv)
         force_hf_warp = (-local_e_deriv_warp)
 
         # compute psi derivative
-        psilogderiv = (np.log(np.abs(psi_sec)) - np.log(np.abs(psi))) / da
-        psilogderiv_warp = (np.log(np.abs(psi_sec_warp)) - np.log(np.abs(psi))) / da
+        #psilogderiv = (np.log(np.abs(psi_sec)) - np.log(np.abs(psi))) / da
+        #psilogderiv_warp = (np.log(np.abs(psi_sec_warp)) - np.log(np.abs(psi))) / da
+        psilogderiv = data["grad_a Log Psi"]
+        psilogderiv_warp = data["grad_a Log Psi (warp)"]
 
-        # Jacobian derivative
-        jac_deriv = logjac/da
+        el_times_psilogderiv = data["E_L * grad_a Log Psi"]
+        el_times_psilogderiv_warp = data["E_L * grad_a Log Psi (warp)"]
 
         # pulay force
         force_pulay_exact = (-(local_energy - energy) \
@@ -197,11 +200,16 @@ class DMCForcesInput:
         force_pulay_exact_warp = (-(local_energy - energy) \
             * ((tsec_warp - t)/da + (ssec_warp - s)/da + jac_partial_sum/da))
 
-        force_pulay_vd = (-(local_energy - energy) \
-            * (2*psilogderiv + (ssec - s)/da))
+        #force_pulay_vd = -(local_energy - energy) \
+        #    * (2*psilogderiv + (ssec - s)/da)
+        force_pulay_vd = -((local_energy - energy) * (ssec - s)/da \
+            + 2 * (el_times_psilogderiv - energy*psilogderiv))
 
-        force_pulay_vd_warp = (-(local_energy - energy) \
-            * (2*psilogderiv_warp + (ssec_warp - s)/da + jac_deriv))
+        #force_pulay_vd_warp = (-(local_energy - energy) \
+        #    * (2*psilogderiv_warp + (ssec_warp - s)/da + jac_deriv))
+        force_pulay_vd_warp = -((local_energy - energy) * (ssec_warp - s)/da \
+            + el_times_jac_logderiv - energy * jac_logderiv \
+            + 2 * (el_times_psilogderiv_warp - energy*psilogderiv_warp))
 
         return force_hf, \
                force_hf_warp, \

pydmc/output.py
@@ -68,15 +68,21 @@ class DMCLogger:
         self._ensemble_data = {
                 "Weight": [],
                 "Psi": [],
-                "Gradpsi": [],
                 "Psi (secondary)": [],
-                "Gradpsi (secondary)": [],
                 "Log Jacobian": [],
                 "Sum Log Jacobian": [],
                 "Psi (secondary, warp)": [],
                 "Local energy": [],
                 "Local energy (secondary)": [],
                 "Local energy (secondary, warp)": [],
+                "grad_a E_L": [],
+                "grad_a E_L (warp)": [],
+                "grad_a Log Psi": [],
+                "grad_a Log Psi (warp)": [],
+                "grad_a Log Jacobian": [],
+                "E_L * grad_a Log Jacobian": [],
+                "E_L * grad_a Log Psi": [],
+                "E_L * grad_a Log Psi (warp)": [],
                 "S": [],
                 "T": [],
                 "S (secondary)": [],
@@ -176,9 +182,6 @@ class DMCLogger:
         self._ensemble_data["Psi (secondary)"].append(psisec_val)
         self._ensemble_data["Psi (secondary, warp)"].append(psisec_val_warp)
 
-        self._ensemble_data["Gradpsi"].append(psigrad)
-        self._ensemble_data["Gradpsi (secondary)"].append(psisec_grad)
-
         self._ensemble_data["Log Jacobian"].append(math.log(abs(jac)))
         self._ensemble_data["Sum Log Jacobian"].append(math.log(abs(jac)))
 
@@ -193,14 +196,23 @@ class DMCLogger:
         self._ensemble_data["S (secondary, warp)"].append(ssec_warp)
         self._ensemble_data["T (secondary, warp)"].append(tsec_warp)
 
-        #self._outfile.write(str(outdict) + "\n")
+        self._ensemble_data["grad_a E_L"].append((eloc_prime - eloc) / self._da)
+        self._ensemble_data["grad_a E_L (warp)"].append((eloc_prime_warp - eloc) / self._da)
+
+        self._ensemble_data["grad_a Log Psi"].append((math.log(abs(psisec_val)) - math.log(abs(psival))) / self._da)
+        self._ensemble_data["grad_a Log Psi (warp)"].append((math.log(abs(psisec_val_warp)) - math.log(abs(psival))) / self._da)
+
+        self._ensemble_data["E_L * grad_a Log Psi"].append(eloc * (math.log(abs(psisec_val)) - math.log(abs(psival))) / self._da)
+        self._ensemble_data["E_L * grad_a Log Psi (warp)"].append(eloc * (math.log(abs(psisec_val_warp)) - math.log(abs(psival))) / self._da)
+
+        self._ensemble_data["grad_a Log Jacobian"].append(math.log(abs(jac)) / self._da)
+        self._ensemble_data["E_L * grad_a Log Jacobian"].append(eloc * math.log(abs(jac)) / self._da)
 
     def output(self):
         weights = np.array(self._ensemble_data["Weight"])
         # average all collected data over the ensemble
         for key, value in self._ensemble_data.items():
             self._ensemble_data[key] = np.average(value, weights=weights, axis=0)
-            #self._ensemble_data[key] = np.mean(value)
 
         # for S, T and J: save a partial history of ensemble averages
         for key in self._histories: